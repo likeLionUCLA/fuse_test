88949e065764765b69ec245c825147a7acecee24
greenyim <greenyim@likelion.org>
2017-06-06 20:09:33 -0700

    fuse test

---
+++ greenyim.js
@@ -0,0 +1,104 @@
+// Q1
+let myarr = [1,2,3,4,5];
+
+let newarr = myarr.map(function(ele){
+  console.log(11,12,13,14,15);
+});
+
+
+// Q2
+> `MainView.js`
+
+    var Observable = require("FuseJS/Observable");
+    var myHikes = Observable("hikes");
+
+    var hike = Observable();
+
+// Q3
+>`hikes.js`
+
+    var hikes = [
+       {
+        id: 0,
+        name: "Tricky Trails",
+        location: "Lakebed, Utah",
+        distance: 10.4,
+        rating: 4,
+        comments: "This hike was nice .."
+       },
+       {
+        id: 1,
+        name: "Mondo Mountains",
+        location: "Black Hills, South Dakota",
+        distance: 20.86,
+        rating: 3,
+        comments: "Not the best..."
+       },
+       {
+        id: 2,
+        name: "Pesky Peaks",
+        location: "Bergenhagen, Norway",
+        distance: 8.2,
+        rating: 5,
+        comments: "Short but SO sweet!!"
+       }
+    ];
+
+  module.exports = {
+      id: id,
+      name: name,
+      location: location,
+      distance: distance,
+      rating: rating,
+      comments: comments
+    };
+
+
+  }
+
+//Q4
+Observable observes and automatically updates any change that occurs in the JS or ux file
+
+//Q5
+Fuse Create
+
+//Q6
+Fuse Open..?
+//Q7
+E
+
+//Q8
+<App> :0
+<ClientPanel> : 0
+<JavaScript> : 1
+<StackPanel> : 0
+<Text> : 6
+<TextBox> : 5
+<Button> : 0
+
+//Q9 - 11
+** Q9** <Text/> Apple
+** Q10** <TextBox/> Banana
+** Q11** <TextView/> Orange
+
+//Q12 - 17
+Q12 (a) InputHint
+Q13 (b) Decimal
+Q14 (c) InputHint
+Q15 (d) Integer/Decimal
+Q16 (e) TextWrap?
+Q17 (f) TextWrap?
+    (g) Clicked (Extra credit!)
+
+//Q18
+StackPanel makes the otherwise stacked boxes be displayed on screen.
+If we have a white square, red square, and a blue square (all same sizes), the user
+will only see the white square. We need to use StackPanel so that all three
+squares show up on the screen in a vertical line.
+
+//Q19
+var nameArr = [{var hikes?}]
+
+<Each Item = "{nameArr}">
+	<Text Value = "{name}" />
+</Each>
