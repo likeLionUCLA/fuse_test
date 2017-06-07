68b0ab4c80406569d07e75dfd408e8d8babc01df
Junyoung Kim <jkim7307@gmail.com>
2017-06-06 20:12:20 -0700

    initial

---
+++ junyoung.js
@@ -0,0 +1,83 @@
+//Make sure rename this file.
+
+//Q1
+
+	let myarr = [1,2,3,4,5];
+	var plusTen = myarr.map(function(num) {
+	  return num + 10;
+	});
+
+
+//Q2
+
+    var Observable = require("FuseJS/Observable");
+    var myHikes = require("hikes");
+
+//Q3
+
+{
+    id: 3,
+    name: "Bukhansan",
+    location: "Seoul, South Korea",
+    distance: 10.2,
+    rating: 5,
+    comments: "Good view"
+   }
+
+//04
+ Observable is 
+
+//05
+Fuse (......)
+
+//06
+
+Fuse preivew
+
+//07
+d. MainView.ux
+
+// 08
+ - `<App>` : 0
+ - `<ClientPanel>` : 1
+ - `<JavaScript>` : 1
+ - `<StackPanel>` : 1
+ - `<Text>` : 1
+ - `<TextBox>` : 1
+ - `<Button>` : 1
+
+ //09
+Apple
+
+ //10
+banana
+
+ //11
+orange
+
+//12
+inputHint 
+
+//13
+flout 
+
+//14
+inputHint
+
+//15
+Integer
+
+//16
+inputHint
+
+//17
+wrap
+
+
+//18
+StackPanel is positioning the code on the screen 
+
+//19
+
+let myarr = require ("hikes")
+myarr.each(hikes)
