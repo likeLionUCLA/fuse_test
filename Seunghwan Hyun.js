e2055d52eb0fd31d5b7f4355edcbd4fb91d68618
hyunjerry <hyunjerry@hotmail.com>
2017-06-06 20:22:49 -0700

    seunghwan

---
+++ Seunghwan Hyun.js
@@ -0,0 +1,76 @@
+// Make sure rename this file.
+
+// Q1
+add = function(x){
+  x = x+10
+}
+
+ let myarr = [1,2,3,4,5];
+ myarr = myarr.map(add)
+
+// Q2
+require
+
+// Q3
+
+// Q4
+//When I save fuse file, it automatically updates fuse preview without extra effort.
+
+// Q5
+// fuse create app name
+
+// Q6
+// fuse preview
+
+// Q7
+// D
+
+// Q8
+//<App> : 0
+//<ClientPanel> : 0
+//<JavaScript> : 1
+//<StackPanel> : 0
+//<Text> : 6
+//<TextBox> : 6
+//<Button>: 1
+
+// Q9
+// <Text apple />
+
+// Q10
+// <TextBox banana />
+
+// Q11
+// <TextView orange />
+
+// Q12
+// getHint : it specifies the type of input value from user
+
+// Q13
+// integer : it specifies the type of input would be integer
+
+// Q14
+// getHint : it specifies the type of input value from user
+
+// Q15
+// integer : it specifies the type of input would be integer
+
+// Q16
+// getHint : it specifies the type of input value from user
+
+// Q17
+// string : it specifies the type of input would be string
+
+// Extra Credit
+// (g) = clicked
+
+// Q18
+// Without stackpanel, if I make three div of size 100px by 100px of color white, red and blue, they would stack up in same position and user would only see the white box.
+// However, with stackpanel, user could see three boxes horizontally placed.
+
+// Q19
+get = function(x){
+  return(x.name);
+}
+
+ var nameArr = hikes.ForEach(get)
